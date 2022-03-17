a42fa6297a1c27321e48e1ec16739541fa8fe7bc
Jicaffo <jicaffo@yahoo.com>
2022-03-17 13:42:55 -0300

    Comentarios sobre posible error en conexión a resolver

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const URI = 'mongodb://localhost/mern-crud-test';
 
+//Hay algun error con la conexión, parecería ser algo de Windows (él utiliza Linux), o de no tener correctamente instalado MongoDB
 mongoose.connect(URI)
   .then(db => console.log('Db is connected'))
   .catch(error => console.error(error));
